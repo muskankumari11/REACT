6b63ab10c21ee4d85a7a46f0bf4d044dbf4e7583
muskankumari11 <111870054+muskankumari11@users.noreply.github.com>
2024-05-09 16:18:05 +0530

    Create PostCard.jsx

---
+++ 15). 12MegaBlog_AW/Src/components/PostCard.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import appwriteService from "../appwrite/config"
+import {Link} from 'react-router-domb'
+function PostCard({$id, title, featuredImage}) {
+    
+  return (
+    <Link to={`/post/${$id}`}>
+        //link ke andar kya display karna h
+        <div className='w-full bg-gray-100 rounded-xl p-4'>
+ <div className='w-full justify-center mb-4'>
+    <img src={appwriteService.getFilePreview
+        (featuredImage)} alt={title}
+        className='rounded-xl'/>
+    
+    </div>
+    <h2
+    className='text-xl font-bold'
+    j>{title}</h2>
+    </div>
+    </Link>
+  )
+}
+
+export default PostCard
