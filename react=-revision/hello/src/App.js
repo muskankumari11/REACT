4b972d0b75d116ba57fa97296fa8c31c4e969f5b
muskankumari11 <111870054+muskankumari11@users.noreply.github.com>
2025-07-21 15:15:12 +0530

    Create App.js

---
+++ react=-revision/hello/src/App.js
@@ -0,0 +1,21 @@
+
+
+// function App() {
+//   return (
+//       <h1>Hello World</h1>
+//   );
+// }
+
+// export default App;
+
+import first from './first'
+function App(){
+return(
+    <>
+    <first/>
+    <h1>its first one</h1>
+    </>
+);
+}
+
+export default App;
