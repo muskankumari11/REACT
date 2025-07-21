500e0f2995f6da9513849d20b2ff36517f010f9e
muskankumari11 <111870054+muskankumari11@users.noreply.github.com>
2025-07-21 15:30:04 +0530

    Create App.jsx

---
+++ react=-revision/vite-hello/understand react flow structure/App.jsx
@@ -0,0 +1,14 @@
+import First from "./first"
+function App() {
+
+
+  return (
+    <div>
+          <First/>
+          <h1>can return more than one using div</h1>
+          <p>test paragraph</p>
+          </div>
+    )
+}
+
+export default App
